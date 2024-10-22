a28a111f822801d5602b1eba890edf4ea946a9cd
LucasBilbao <lucasbm789@gmail.com>
2024-10-22 12:45:50 +0200

    feat: add constants

---
+++ Constants.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef Constants_h
+#define Constants_h
+
+#define PLAYER_1_RED_LED_PIN 13
+#define PLAYER_1_GREEN_LED_PIN 12
+#define PLAYER_1_BUTTON_PIN 11
+
+#define PLAYER_2_RED_LED_PIN 10
+#define PLAYER_2_GREEN_LED_PIN 9
+#define PLAYER_2_BUTTON_PIN 8
+
+#define PLAYER_3_RED_LED_PIN 7
+#define PLAYER_3_GREEN_LED_PIN 6
+#define PLAYER_3_BUTTON_PIN 5
+
+#define PLAYER_4_RED_LED_PIN 4
+#define PLAYER_4_GREEN_LED_PIN 3
+#define PLAYER_4_BUTTON_PIN 2
+
+#define DURATION_UNTIL_GAME_STARTS_MS 10000
+#define DURATION_BEFORE_BLINKING_MS 10000
+#define DURATION_OF_TURN_MS 15000
+
+#define RS 53
+#define E 51
+#define D4 43
+#define D5 45
+#define D6 47
+#define D7 49
+
+#define NUM_OF_QUESTIONS 3
+#endif
